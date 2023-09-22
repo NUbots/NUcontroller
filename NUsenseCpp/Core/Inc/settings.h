74c22644392e558f3c7331361a7024819e97dd25
Clayton <47998944+Claegtun@users.noreply.github.com>
2023-09-22 14:52:54 +1000

    Move the NUsense firmware to the NUcontroller repository. (#2)
    * Moved the NUsense firmware to NUcontroller repository.
    
    * Removed built files.

---
+++ NUsenseCpp/Core/Inc/settings.h
@@ -0,0 +1,29 @@
+/*
+ * settings.h
+ *
+ *  Created on: Oct 21, 2022
+ *      Author: Clayton
+ *      Description:
+ *          A centralised area to enable and disable various functionalities.
+ */
+
+#ifndef INC_SETTINGS_H_
+#define INC_SETTINGS_H_
+
+//#define FIRST_BUZZ
+#define DXL_PWR
+//#define USING_FOOT_SENSORS
+
+#define RUN_MAIN
+
+//#define TEST_UART 1
+
+//#define TEST_USB
+
+//#define TEST_IMU
+
+//#define TEST_PORT
+
+//#define TEST_MOTOR 2
+
+#endif /* INC_SETTINGS_H_ */
