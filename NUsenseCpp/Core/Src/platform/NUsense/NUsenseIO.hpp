7bbdb005e683a2b9fb2dcaae1a8a1bf6fffa963f
Claegtun <clayton.john.carlon@gmail.com>
2024-03-18 15:26:32 +1100

    Fixed whitespaces in one file.

--- NUsenseCpp/Core/Src/platform/NUsense/NUsenseIO.hpp
+++ NUsenseCpp/Core/Src/platform/NUsense/NUsenseIO.hpp
@@ -64,39 +64,39 @@ namespace platform::NUsense {
             // without polling each port. Later we will get polling at start-up so that the 
             // devices do not have to be connected to a specific port.
             chains({
-				std::vector<platform::NUsense::NUgus::ID>{
-						platform::NUsense::NUgus::ID::R_SHOULDER_PITCH,
-						platform::NUsense::NUgus::ID::R_SHOULDER_ROLL,
-						platform::NUsense::NUgus::ID::R_ELBOW
-				},
-				std::vector<platform::NUsense::NUgus::ID>{
-						platform::NUsense::NUgus::ID::L_SHOULDER_PITCH,
-						platform::NUsense::NUgus::ID::L_SHOULDER_ROLL,
-						platform::NUsense::NUgus::ID::L_ELBOW
-				},
-				std::vector<platform::NUsense::NUgus::ID>{
-						platform::NUsense::NUgus::ID::R_HIP_YAW,
-						platform::NUsense::NUgus::ID::R_HIP_ROLL,
-						platform::NUsense::NUgus::ID::R_HIP_PITCH,
-						platform::NUsense::NUgus::ID::R_KNEE,
-						platform::NUsense::NUgus::ID::R_ANKLE_PITCH,
-						platform::NUsense::NUgus::ID::R_ANKLE_ROLL
-				},
-				std::vector<platform::NUsense::NUgus::ID>{
-						platform::NUsense::NUgus::ID::L_HIP_YAW,
-						platform::NUsense::NUgus::ID::L_HIP_ROLL,
-						platform::NUsense::NUgus::ID::L_HIP_PITCH,
-						platform::NUsense::NUgus::ID::L_KNEE,
-						platform::NUsense::NUgus::ID::L_ANKLE_PITCH,
-						platform::NUsense::NUgus::ID::L_ANKLE_ROLL
-				},
-				std::vector<platform::NUsense::NUgus::ID>{
-						platform::NUsense::NUgus::ID::HEAD_YAW,
-						platform::NUsense::NUgus::ID::HEAD_PITCH
-				},
-				std::vector<platform::NUsense::NUgus::ID>{
-				}
-			}),
+                std::vector<platform::NUsense::NUgus::ID>{
+                        platform::NUsense::NUgus::ID::R_SHOULDER_PITCH,
+                        platform::NUsense::NUgus::ID::R_SHOULDER_ROLL,
+                        platform::NUsense::NUgus::ID::R_ELBOW
+                },
+                std::vector<platform::NUsense::NUgus::ID>{
+                        platform::NUsense::NUgus::ID::L_SHOULDER_PITCH,
+                        platform::NUsense::NUgus::ID::L_SHOULDER_ROLL,
+                        platform::NUsense::NUgus::ID::L_ELBOW
+                },
+                std::vector<platform::NUsense::NUgus::ID>{
+                        platform::NUsense::NUgus::ID::R_HIP_YAW,
+                        platform::NUsense::NUgus::ID::R_HIP_ROLL,
+                        platform::NUsense::NUgus::ID::R_HIP_PITCH,
+                        platform::NUsense::NUgus::ID::R_KNEE,
+                        platform::NUsense::NUgus::ID::R_ANKLE_PITCH,
+                        platform::NUsense::NUgus::ID::R_ANKLE_ROLL
+                },
+                std::vector<platform::NUsense::NUgus::ID>{
+                        platform::NUsense::NUgus::ID::L_HIP_YAW,
+                        platform::NUsense::NUgus::ID::L_HIP_ROLL,
+                        platform::NUsense::NUgus::ID::L_HIP_PITCH,
+                        platform::NUsense::NUgus::ID::L_KNEE,
+                        platform::NUsense::NUgus::ID::L_ANKLE_PITCH,
+                        platform::NUsense::NUgus::ID::L_ANKLE_ROLL
+                },
+                std::vector<platform::NUsense::NUgus::ID>{
+                        platform::NUsense::NUgus::ID::HEAD_YAW,
+                        platform::NUsense::NUgus::ID::HEAD_PITCH
+                },
+                std::vector<platform::NUsense::NUgus::ID>{
+                }
+            }),
             // Make a packet-handler for each port.
             packet_handlers({
                 dynamixel::PacketHandler(ports[0]),
