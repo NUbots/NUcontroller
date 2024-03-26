26630e427d20cddf240bc095eceb768ee9adb39c
Dex K <dex.misc@gmail.com>
2024-03-26 14:26:39 +1100

    remove old tests, continues 3965250

--- NUsenseCpp/Core/Inc/settings.h
+++ NUsenseCpp/Core/Inc/settings.h
@@ -12,18 +12,8 @@
 
 #define FIRST_BUZZ
 // #define DXL_PWR
-// #define USING_FOOT_SENSORS
 
 // #define RUN_MAIN
-
-// #define TEST_UART 1
-
-// #define TEST_USB
-
 // #define TEST_IMU
-
-// #define TEST_PORT
-
-// #define TEST_MOTOR 2
 
 #endif /* INC_SETTINGS_H_ */
