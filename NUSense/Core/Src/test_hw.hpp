6edb9080e2ff5a4bb323d8b41488236d45b980dc
Ysobel Sims <35280100+ysims@users.noreply.github.com>
2024-04-18 12:03:08 +1000

    Change NUsense to NUSense, NUSenseCpp to NUSense, some typo fixes, formatting, other stylistic changes (#12)
    * Made the class for a millisecond-timer that counts the SysTick variable; fixed some things in MicrosecondTimer.
    
    * Added a Button class that reads any GPIO pin and also has debouncing; removed debugging signals; fixed some other stuff.
    
    * Added a circular mean to ServoState class; filtered the servo-states by a simple average.
    
    * Added a state for the other hardware stuff on the board, etc.
    
    * Tidied with clang in order to make the merge less messy.
    
    * Fixed a backwards stroke in the gitignore.
    
    * Fixed merge, the code for this branch seems to work well.
    
    * Integrated the protobuf encoding with the 100-Hz loop and the averaging filter; got upstream communiction to work mostly well; the filter seemed fine when I changed the voltage.
    
    * Made the gyroscope's values in radians per second instead of degrees.
    
    * Fixed a bug where the USB packet-handler would die if there was a discrepancy between the callback's flag and the actual size of the ring-buffer.
    
    * Removed adding fake offsets that were buggy; reset the circular mean properly; added a made up profile-velocity as a quick hack.
    
    * Fixed the chains since they were somehow changed back to the old arrangement; added a default profile-velocity of 1000 ms on startup.
    
    * Fixed the bug where the servos moved quickly when the torque was disabled by adding a small delay between packets; added the new time-span from HardwareIO to the profile-velocity.
    
    * Negate y and z axis to account for nusense positioning
    
    * Set the optimisation to -Ofast.
    
    * add formatting script
    
    * NUSense
    
    * NUSense
    
    * cleaning
    
    * format
    
    * rename
    
    * format
    
    * just call it nusense?
    
    * change name
    
    * fix merge
    
    * compile
    
    * comments
    
    * :)
    
    * review and add imu.h to format
    
    * imu.h to format
    
    ---------
    
    Co-authored-by: Claegtun <clayton.john.carlon@gmail.com>

---
+++ NUSense/Core/Src/test_hw.hpp
@@ -0,0 +1,135 @@
+/*
+ * test_hw.hpp
+ *
+ *  Created on: 26 Feb. 2023
+ *      Author: Clayton
+ */
+
+#ifndef SRC_TEST_HW_HPP_
+#define SRC_TEST_HW_HPP_
+
+#include "imu.h"
+#include "settings.h"
+#include "stm32h7xx_hal.h"
+#include "usbd_cdc_if.h"
+
+namespace test_hw {
+
+#ifdef TEST_IMU
+    void imu() {
+        // Create our IMU instance
+        NUSense::IMU imu{};
+        // structs to hold data
+        NUSense::IMU::RawData raw_data;
+        NUSense::IMU::ConvertedData converted_data;
+
+        // variables for testing
+        uint8_t rx[14];
+        char str[256];
+        uint8_t error = 0;
+
+        // lets start this baby
+        imu.init();
+
+        while (1) {
+            /* test readReg */
+            rx[0] = 0xFF;  // clear to known state
+            imu.readReg(NUSense::IMU::Address::WHO_AM_I, rx);
+            if (rx[0] != 0x98) {
+                error++;
+            }
+
+            /* test readBurst with a single readval */
+            rx[0] = 0xFF;  // clear to known state
+            imu.readBurst(NUSense::IMU::Address::WHO_AM_I, rx, 1);
+            if (rx[0] != 0x98) {
+                error++;
+            }
+
+            /* test readBurst with multiple readvals */
+            /* NOTE this one fails and im not sure why... */
+            for (int i = 0; i < 14; i++)
+                rx[i] = 0xFF;                                       // clear to known state
+            imu.readBurst(NUSense::IMU::Address::FIFO_R_W, rx, 3);  // include reg either side of WHO AM I
+            if (rx[1] != 0x98) {
+                error++;
+            }
+
+
+            /* test just acc x vals */
+            for (int i = 0; i < 14; i++)
+                rx[i] = 0xFF;  // clear to known state
+            imu.readBurst(NUSense::IMU::Address::ACCEL_XOUT_H, rx, 2);
+            uint16_t acc_x_raw = (rx[0] << 8) | rx[1];                                          // swap byte order
+            float acc_x_conv   = static_cast<float>(acc_x_raw) / imu.ACCEL_SENSITIVITY_CHOSEN;  // conversion
+
+            /* test all sensor vals */
+            for (int i = 0; i < 14; i++)
+                rx[i] = 0xFF;  // clear to known state
+            imu.readBurst(imu.READ_BLOCK_START, rx, imu.READ_BLOCK_LEN);
+            raw_data = *(reinterpret_cast<NUSense::IMU::RawData*>(rx));  // cast raw bytes to struct
+            imu.convertRawData(&raw_data, &converted_data);
+
+            /* test class form, resets form breakpoint locations */
+
+            // reset vals to be sure;
+            raw_data       = {};
+            converted_data = {};
+            // check with first raw data generation
+            raw_data = imu.getNewRawData();
+            imu.generateConvertedData();
+            converted_data = imu.getLastConvertedData();
+
+            // reset again
+            raw_data       = {};
+            converted_data = {};
+            // check with opposite of before. the vals here shouldn't align
+            raw_data       = imu.getLastRawData();
+            converted_data = imu.getNewConvertedData();
+
+            // reset one last time
+            raw_data       = {};
+            converted_data = {};
+            // check if read on demand works
+            raw_data = imu.getLastRawData();  // read data
+            imu.getNewRawData();              // get fresh data
+            raw_data = imu.getLastRawData();  // read it again
+
+            sprintf(str,
+                    "IMU:\t"
+                    "ACC (g):\t%.3f\t%.3f\t%.3f\t"
+                    "TEMP (deg C):\t%.3f\t"
+                    "GYR (dps):\t%.3f\t%.3f\t%.3f\t"
+                    "Raw:\t%x%x\t%x%x\t%x%x\t%x%x\t%x%x\t%x%x\t%x%x\r\n",
+                    converted_data.accelerometer.x,
+                    converted_data.accelerometer.y,
+                    converted_data.accelerometer.z,
+                    converted_data.temperature,
+                    converted_data.gyroscope.x,
+                    converted_data.gyroscope.y,
+                    converted_data.gyroscope.z,
+                    raw_data.accelerometer.x.h,
+                    raw_data.accelerometer.x.l,
+                    raw_data.accelerometer.y.h,
+                    raw_data.accelerometer.y.l,
+                    raw_data.accelerometer.z.h,
+                    raw_data.accelerometer.z.l,
+                    raw_data.temperature.h,
+                    raw_data.temperature.l,
+                    raw_data.gyroscope.x.h,
+                    raw_data.gyroscope.x.l,
+                    raw_data.gyroscope.y.h,
+                    raw_data.gyroscope.y.l,
+                    raw_data.gyroscope.z.h,
+                    raw_data.gyroscope.z.l);
+
+            CDC_Transmit_HS((uint8_t*) str, strlen(str));
+
+            HAL_Delay(100);
+        }
+    }
+#endif
+
+}  // namespace test_hw
+
+#endif /* SRC_TEST_HW_HPP_ */
---
+++ format.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+#
+# MIT License
+#
+# Copyright (c) 2017 NUbots
+#
+# This file is part of the NUbots codebase.
+# See https://github.com/NUbots/NUbots for further info.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import argparse
+import datetime
+import multiprocessing
+import os
+import shutil
+import sys
+import tempfile
+from collections import OrderedDict
+from fnmatch import fnmatch
+from functools import partial
+from subprocess import DEVNULL, PIPE, STDOUT, CalledProcessError
+from subprocess import run as sp_run
+
+import pygit2
+
+repo = pygit2.Repository(".")
+
+# The extensions that are handled by the various formatters
+formatters = OrderedDict()
+
+formatters["clang-format"] = {
+    "format": [["clang-format", "-i", "-style=file", "{path}"]],
+    "include": ["*.cpp", "*.hpp", "*.proto", "NUSense/Core/Inc/imu.h"],
+    "exclude": [
+        "NUSense/Drivers/*",
+        "NUSense/USB_DEVICE/*",
+        "NUSense/Middlewares/*",
+    ],
+}
+formatters["isort"] = {
+    "format": [["isort", "--quiet", "{path}"]],
+    "include": ["*.py"],
+    "exclude": [],
+}
+formatters["black"] = {
+    "format": [["black", "--quiet", "{path}"]],
+    "include": ["*.py"],
+    "exclude": [],
+}
+formatters["prettier"] = {
+    "format": [["prettier", "--write", "{path}"]],
+    "include": ["*.md", "*.yaml", "*.yml"],
+    "exclude": [],
+}
+
+# Format or check the file
+def _do_format(path, verbose, check=True):
+    text = ""
+    success = True
+    output_path = None
+    try:
+        # Find the correct formatter and format the file
+        formatter = []
+        formatter_names = []
+        for name, fmt in formatters.items():
+            if (any(fnmatch(path, pattern) for pattern in fmt["include"])) and (
+                all(not fnmatch(path, pattern) for pattern in fmt["exclude"])
+            ):
+                formatter_names.append(name)
+                formatter.extend(fmt["format"])
+
+        # If we don't have a formatter then skip this file
+        if len(formatter) == 0:
+            return (
+                f"Skipping {path} as it does not match any of the formatters\n"
+                if verbose >= 1
+                else "",
+                True,
+            )
+
+        text = f"Formatting {path} with {', '.join(formatter_names)}\n"
+
+        tmp_dir = tempfile.TemporaryDirectory(dir=os.path.dirname(path))
+
+        # Make a copy of the file to do the formatting on
+        output_path = os.path.join(tmp_dir.name, os.path.basename(path))
+        shutil.copy(path, output_path)
+
+        # Apply our arguments to the formatter command
+        args = {"path": output_path}
+        formatter = [[arg.format(**args) for arg in tool] for tool in formatter]
+
+        # Format the code
+        tool_text = ""
+        run_args = {"stderr": STDOUT, "stdout": PIPE, "check": True}
+        for c in formatter:
+            # Print the command being executed
+            if verbose >= 2:
+                text += f"\t$ {' '.join(c)}\n"
+            cmd = sp_run(c, **run_args)
+            tool_text = tool_text + cmd.stdout.decode("utf-8")
+
+        if verbose >= 1 and tool_text:
+            text = text + tool_text
+
+        if check:
+            # Run the diff command
+            cmd = sp_run(
+                ["colordiff", "--color=yes", "--unified", path, output_path], **run_args
+            )
+        else:
+            # Check if the file has changed and if so replace the original using python
+            with open(path, "rb") as f:
+                with open(output_path, "rb") as g:
+                    if f.read() != g.read():
+                        os.rename(output_path, path)
+
+    except CalledProcessError as e:
+        text = text + e.output.decode("utf-8").strip()
+        success = False
+
+    return text, success
+
+
+def run(verbose, check, format_all, globs):
+    # Use git to get all of the files that are committed to the repository or just the ones that are different to main
+    if format_all:
+        files = (
+            sp_run(["git", "ls-files"], stdout=PIPE, check=True)
+            .stdout.decode("utf-8")
+            .splitlines()
+        )
+    else:
+        files = (
+            sp_run(
+                ["git", "diff", "--name-only", "origin/main"], stdout=PIPE, check=True
+            )
+            .stdout.decode("utf-8")
+            .splitlines()
+        )
+
+    # Filter to a list containing only existing files (git diff can return deleted files)
+    files = [f for f in files if os.path.isfile(f)]
+
+    # Filter the files we found by any globs we are using
+    if len(globs) != 0:
+
+        def matches_glob(f):
+            for g in globs:
+                if fnmatch(f, g):
+                    return True
+            return False
+
+        files = filter(matches_glob, files)
+
+    success = True
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        for r, s in pool.imap_unordered(
+            partial(_do_format, verbose=verbose, check=check), files
+        ):
+            sys.stdout.write(r)
+            success = success and s
+
+    sys.exit(0 if success else 1)
+
+
+arg_parser = argparse.ArgumentParser(description="Format code in a repository")
+arg_parser.add_argument(
+    "--verbose",
+    "-v",
+    action="count",
+    default=0,
+    help="Print more information about what is happening",
+)
+arg_parser.add_argument(
+    "--check",
+    "-c",
+    action="store_true",
+    help="Check if the code is formatted correctly",
+)
+arg_parser.add_argument(
+    "--all",
+    "-a",
+    action="store_true",
+    help="Format all files in the repository",
+)
+
+arg_parser.add_argument(
+    "globs",
+    nargs="*",
+    help="Globs to filter the files that are formatted",
+)
+
+args = arg_parser.parse_args()
+
+run(args.verbose, args.check, args.all, args.globs)
