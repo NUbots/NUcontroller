1a0243fd87fb746f69603962fd5fa713a4478ea4
Miles Punch <miles.anyprintz@gmail.com>
2026-08-05 09:27:29 +1000

    Revert "this should fail CI/CD as an unused variable warning"
    This reverts commit d1fb21f82dc0e5e6b42a35003f08bd16d104b2e5.

--- NUSense/Core/Src/main.cpp
+++ NUSense/Core/Src/main.cpp
@@ -42,8 +42,6 @@ void SystemClock_Config(void);
 int main(void) {
     /* MCU Configuration--------------------------------------------------------*/
 
-    int unused_variable = 10;
-
     /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
     HAL_Init();
 
